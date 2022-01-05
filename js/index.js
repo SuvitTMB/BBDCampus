a7123431b54e0d0a558e8e990de724571ffdb7a9
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2022-01-05 21:29:05 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -24,13 +24,13 @@ var sDateTime = "";
 var sLINERegister = "";
 
 $(document).ready(function () {
-  var sLineID = "Ua6b6bf745bd9bfd01a180de1a05c23b3";
-  var sLineName = "Website";
-  var sLinePicture = "https://profile.line-scdn.net/0hoLlg-mNNMGNRHiaTpMdPNG1bPg4mMDYrKX8qVnIYOgYpe3QwbCp2AXVKaVN_fnMzOC16V3NMagF8";
-  sessionStorage.setItem("LineID", sLineID);
-  sessionStorage.setItem("LineName", sLineName);
-  sessionStorage.setItem("LinePicture", sLinePicture);
-  //main()
+  //var sLineID = "Ua6b6bf745bd9bfd01a180de1a05c23b3";
+  //var sLineName = "Website";
+  //var sLinePicture = "https://profile.line-scdn.net/0hoLlg-mNNMGNRHiaTpMdPNG1bPg4mMDYrKX8qVnIYOgYpe3QwbCp2AXVKaVN_fnMzOC16V3NMagF8";
+  //sessionStorage.setItem("LineID", sLineID);
+  //sessionStorage.setItem("LineName", sLineName);
+  //sessionStorage.setItem("LinePicture", sLinePicture);
+  main()
   Connect_DB();
   CheckBootCampOpen();
   //CheckRegister();
