bafa44297049fe6d1cef8e523aa27c17c2f7238e
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2022-01-11 10:10:25 +0700

    Add files via upload

--- js/rasc-workshop.js
+++ js/rasc-workshop.js
@@ -13,7 +13,7 @@ var Eid = "";
 var EidBootCamp = "";
 var EidBootRegister = "";
 var EidBootMember = "";
-var dateString = "";
+  dateString = new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' });
 var sDateRegister = "";
 var i = 0;
 const x = document.querySelectorAll(`div.com[min="${i}"]`);
@@ -334,7 +334,6 @@ function SaveData() {
   var eEmpGroup = "other";
   NewDate();
   var TimeStampDate = Math.round(Date.now() / 1000);
-  dateString = new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' });
   sDateTime = dateString;
   sessionStorage.setItem("CheckPass", sDateTime);
   if(document.getElementById("txtEmpGroup").value!="OTHER") {
@@ -523,6 +522,36 @@ function phone_number_check(field,e) {
   }
 
 }
+
+function NewDate() {
+  var today = new Date();
+  var day = today.getDate() + "";
+  var month = (today.getMonth() + 1) + "";
+  var year = today.getFullYear() + "";
+  var hour = today.getHours() + "";
+  var minutes = today.getMinutes() + "";
+  var seconds = today.getSeconds() + "";
+  var ampm = hour >= 12 ? 'PM' : 'AM';
+
+  day = checkZero(day);
+  month = checkZero(month);
+  year = checkZero(year);
+  hour = checkZero(hour);
+  minutes = checkZero(minutes);
+  seconds = checkZero(seconds);
+
+  dateString = day + "/" + month + "/" + (parseInt(year)+543) + " " + hour + ":" + minutes + ":" + seconds +" "+ ampm;
+  //alert(GetNewDate);
+  //console.log(day + "/" + month + "/" + year + " " + hour + ":" + minutes + ":" + seconds +" "+ ampm);
+}
+
+function checkZero(data){
+  if(data.length == 1){
+    data = "0" + data;
+  }
+  return data;
+}
+
 
 //document.getElementById('txtEmpPhone').onkeyup = function(e) {
 //  phone_number_check(this,e);
