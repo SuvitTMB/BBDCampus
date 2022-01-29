f09d29652430638306971321e192eea00260ea3d
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2022-01-29 12:28:09 +0700

    Update round3Day2.js

--- js/round3Day2.js
+++ js/round3Day2.js
@@ -30,7 +30,7 @@ parts = xRound.split("-"); //สร้างString arry ชื่อparts
 var FinalRound = parts[0]; // 004  //String part1 เก็บค่าparts[0]
 var FinalRoundSplit = parts[1]; // 004  //String part1 เก็บค่าparts[0]
 
-$(document).ready(function () {
+$(document).ready(function () { 
   //sessionStorage.clear();
   sessionStorage.setItem("EmpTable", 0);
   sessionStorage.setItem("EmpSize", '');
