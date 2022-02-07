ad89879f97d6b7f7adf6333c53a6c96461e533c0
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2022-02-07 20:08:31 +0700

    Update round8Day1-pre.js

--- js/round8Day1-pre.js
+++ js/round8Day1-pre.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
 
   /*
   var str = "";
-  var sLineID = "Ua6b6bf745bd9bfd01a180de1a05c23b3";
+  var sLineID = "Ua6b6bf745bd9bfd01a180de1a05c23b3"; 
   var sLineName = "Website";
   var sLinePicture = "https://profile.line-scdn.net/0hoLlg-mNNMGNRHiaTpMdPNG1bPg4mMDYrKX8qVnIYOgYpe3QwbCp2AXVKaVN_fnMzOC16V3NMagF8";
   sessionStorage.setItem("LineID", sLineID);
